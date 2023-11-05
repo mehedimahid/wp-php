6584a427023d067e52aa278b43842124217ffe89
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2023-11-05 19:07:51 +0600

    Loop

--- php/index.php
+++ php/index.php
@@ -1,80 +1,43 @@
 <?php
-$t = date("H");
-echo "<p>The hour (of the server) is " . $t; 
-echo ", and will give the following message:</p>";
-if ($t < "10") {
-  echo "Have a good morning!";
-} elseif ($t < "20") {
-  echo "Have a good day!";
-} else {
-  echo "Have a good night!";
+$cars = array('Volvo', "BMW", "Toyoto");
+$carsLength = count($cars);
+echo("I love ". $cars[0]. "," . $cars[1]. "," . $cars[2] . "<br>");
+for($i = 0; $i< $carsLength; $i++){
+echo($cars[$i]);
+echo("<br>");
 }
 ?>
 
 <?php
-$favCar = 'Ferrari';
-switch ($favCar) {
-  case 'BMW':
-    echo "Your favorite car is BMW";
-    break;
-  case 'Audi':
-    echo "Your favorite car is Audi";
-    break;
-  case 'Ferrari':
-    echo "Your favorite car is Ferrari";
-    break;
-   case 'Bugatti':
-    echo "Your favorite car is Bugatti";
-    break;
-  
-  default:
-    echo "Your favorite car is neither BMW, Audi,Ferrari nor Bugatti!";
-    break;
-}
-?>
+ $age = array("Mehedi"=>"23", "Ben"=>"30","Forhad" =>"22", "Ahosan" =>"23");
+ echo("Mehedi is ". $age["Mehedi"] . " years old ." . "<br>");
 
-<?php
- $x = 1;
- while ($x <= 10) {
- echo("the number is: $x  <br>");
- $x++;
+ foreach($age as $x=>$value){
+  echo $x ." is " . $value . " years old .";
+  echo "<br>";
  }
 ?>
-<?php
- $x = 1;
- do {
-  echo("the number is: $x  <br>");
-  $x++;
- } while ($x <= 10);
- 
-?>
 
 <?php
-for($a = 0; $a<= 10; $a++){
-  echo("the number is: $a  <br>");
-}
-?>
-<?php
-$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+$cars = array(
+  array("Volvo" ,10 , 25),
+  array("BMW" ,15 , 30),
+  array("Land Rover" ,5 , 20)
+);
+$carsLength = count($cars);
 
-foreach($age as $x =>$val) {
-  echo "$x=> $val<br>";
-}
-?>
+echo $cars[0][0] . ": In Stock :" . $cars[0][1] . ", Sold :" . $cars[0][2] . "<br>";
+// echo $cars[1][0] . ": In Stock :" . $cars[1][1] . ", Sold :" . $cars[1][2]. "<br>";
+// echo $cars[2][0] . ": In Stock :" . $cars[2][1] . ", Sold :" . $cars[2][2]. "<br>";
 
-<?php
-for ($i=0; $i <10 ; $i++) { 
-  if ($i ==5) {
-    break;//break hare
-  }
-  echo("this is a break statement. number $i <br>");
+for($i =0; $i< 3; $i++){
+echo "<p><b>Row number $i</b></p>";
+echo("<ul>");
+for($j = 0; $j<3; $j++){
+   echo "<li>".$cars[$i][$j]."</li>";
+  
 }
-?>
-<?php
-for ($i=0; $i <10 ; $i++) { 
-  if ($i ==5) {
-    continue;//skip 5 
-  }
-  echo("this is a continue statement. number $i <br>");
+echo("</ul>");
 }
+
 ?>
